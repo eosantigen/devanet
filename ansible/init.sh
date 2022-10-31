21ecb2c7f792aa8e0cabec2d33ef6d138d36d8f1
Eos Antigen <eos.antigen@gmail.com>
2022-10-31 08:29:22 +0200

    Configured bridged networking
    (not tested yet)

--- ansible/init.sh
+++ ansible/init.sh
@@ -2,6 +2,7 @@
 
 # ansible-config init --disabled > /tmp/ansible.cfg
 # ansible -m ping all
+# ansible-galaxy collection install community.libvirt
 ansible-inventory --list
 ansible -m ansible.builtin.setup all --args \
     'filter=all_ipv4_addresses,dns,interfaces,network,os_family,python_version,user_shell'
